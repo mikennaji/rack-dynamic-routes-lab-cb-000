8e70d2beaf5d26a41a581c3d6f79eeab888e064d
Michael Nnaji <mnnaji3@uwo.ca>
2020-11-25 09:36:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,14 @@
 class Application
 
+@@items 
+
 def call(env)
  resp = Rack::Response.new
  req = Rack::Request.new(env)
  if req.path.match(/items/)
-
+   item_name= req.path.split("/items/").last
+   item = @@items.find{|s| s.name ==item_name}
+   resp.write "#{item.price}"
  else
    resp.write "Route not found"
    resp.status = 404
