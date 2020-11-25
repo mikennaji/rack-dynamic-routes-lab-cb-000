895d916b037d70a2a7569db84d18417e426743dd
Michael Nnaji <mnnaji3@uwo.ca>
2020-11-25 09:38:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,11 @@ def call(env)
  if req.path.match(/items/)
    item_name= req.path.split("/items/").last
    item = @@items.find{|s| s.name ==item_name}
+   if item==nil
+     resp.status = 400
+   else
    resp.write "#{item.price}"
+ end
  else
    resp.write "Route not found"
    resp.status = 404
