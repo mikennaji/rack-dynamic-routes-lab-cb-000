6c0bd6b031f5b9a26bf173a0084e9520a8565332
Michael Nnaji <mnnaji3@uwo.ca>
2020-11-25 09:32:10 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,14 @@
 class Application
 
 def call(env)
+ resp = Rack::Response.new
+ req = Rack::Request.new(env)
+ if req.path.match(/items/)
 
+ else
+   resp.status = 404
+ end
+ resp.finish
 
 end
 
